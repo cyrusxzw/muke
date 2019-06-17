8dccca0baf5cd23840bf04b7ee766b690311b17e
cyrusxzw <xuzengwei06@gmail.com>
2019-06-17 15:15:43 +1000

    login first commit

--- src/Components/pc_header.js
+++ src/Components/pc_header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Row, Col, Menu, Icon } from 'antd';
+import { Row, Col, Menu, Icon, Tabs, message, Form, Input, Button, Checkbox, Modal} from 'antd';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
 
 
 const Img = styled.img`
@@ -18,15 +19,57 @@ const NavLink = styled(Link)`
     display: flex;
     align-items: center;
 `
-export default class PCHeader extends React.Component{
+class PCHeader extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             current:"top",
+            modalVisible: false,
+            action: 'login',
+            hasLogin: false,
+            userName: "",
+            userid: 0,
         };
     }
 
+    setModalVisible(value){
+        this.setState({
+            modalVisible:value,
+        });
+    }
+
+    handleClick(e){
+        if(e.key == "register"){
+            console.log(e.key);
+            this.setState({
+                current: "register",
+                modalVisible: true,
+            });
+        }
+        else {
+            this.setState({
+                current: e.key,
+            });
+        }
+    }
+
+    handleSubmit(e){
+
+    }
+
     render(){
+        const { TabPane } = Tabs;
+        let {getFieldProps} = this.props.form;
+        const userShow = this.state.hasLogin?<Menu.Item key="logout" className="register">
+            <Button type="primary" htmlType="button">{this.state.userName}</Button>
+            &nbsp;&nbsp;
+            <Link to=""><Button type="dashed" htmlType="button">My Account</Button></Link>
+            &nbsp;&nbsp;
+            <Button type="ghost" htmlType="button">Logout</Button>
+        </Menu.Item>:
+        <Menu.Item key="register" className="register">
+            <Icon type="appstore"/>Register/Login
+        </Menu.Item>;
         return(
         <header>
         <Row>
@@ -38,7 +81,7 @@ export default class PCHeader extends React.Component{
               </NavLink>
           </Col>
           <Col span={16}>
-              <Menu mode="horizontal" selectedKeys={[this.state.current]}>
+              <Menu mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]}>
                 <Menu.Item key="top">
                     <Icon type="appstore"/>Top
                 </Menu.Item>
@@ -60,7 +103,36 @@ export default class PCHeader extends React.Component{
                 <Menu.Item key="shishang">
                     <Icon type="appstore"/>Fashion
                 </Menu.Item>
+                {userShow}
               </Menu>
+            <Modal 
+                title="User" 
+                wrapClassName="vertical-center-modal" 
+                visible={this.state.modalVisible} 
+                onCancel = {()=>this.setModalVisible(false)}
+                onOk = {()=>this.setModalVisible(false)}
+                okText = 'Closed'>
+                <Tabs type="card">
+                    <TabPane tab="Register" key="2">
+                        <Form mode="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+                            <Form.Item>
+                                <Input placeholder="Please input username" {...getFieldProps('r_userName')}/>
+                            </Form.Item>
+                            <Form.Item label="Password">
+                                <Input type="password" placeholder="Please input password" {...getFieldProps('r_password')}/>
+                            </Form.Item>
+                            <Form.Item label="Confirm Password">
+                                <Input type="password" placeholder="Please confirm password" {...getFieldProps('r_confirmPassword')}/>
+                            </Form.Item>
+                            <Button type="primary" htmlType="submit">Register</Button>
+                        </Form>
+                    </TabPane>
+                    
+                </Tabs>    
+
+            </Modal>
+
+
           </Col>
           <Col span={2}></Col>    
       </Row>
@@ -68,3 +140,5 @@ export default class PCHeader extends React.Component{
         )
     }
 }
+
+export default PCHeader = Form.create()(PCHeader);
